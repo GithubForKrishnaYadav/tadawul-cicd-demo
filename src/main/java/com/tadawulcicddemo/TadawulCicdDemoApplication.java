c1a3a4387675c08e8ddb042675242f8846ee7715
Krishna <beakrish@gmail.com>
2023-08-26 17:31:31 +0300

    initial commit

---
+++ src/main/java/com/tadawulcicddemo/TadawulCicdDemoApplication.java
@@ -0,0 +1,14 @@
+package com.tadawulcicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TadawulCicdDemoApplication extends SpringBootServletInitializer {
+//public class TadawulCicdDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TadawulCicdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadawulcicddemo/TadawulCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadawulcicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadawulCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
